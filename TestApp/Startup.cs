9c6393ee6fe4cd9d31a1b7719198b23162e72773
Nathan Rath <nathan@silverchip.com>
2021-06-30 17:50:04 +0100

    Initial Commit

---
+++ TestApp/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace TestApp {
+    public class Startup {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.Configure<KestrelServerOptions>(options => {
+                options.AllowSynchronousIO = true;
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context => await context.Response.SendFileAsync("./wwwroot/index.html"));
+
+                endpoints.MapGet("/test", async context => await context.Response.WriteAsync("Test"));
+
+                endpoints.MapPost("/send", async context => {
+                    var req = context.Request;
+                    var body = await Utils.ParseBody(req.Body);
+                    
+                    Console.WriteLine(body);
+                });
+            });
+            
+        }
+    }
+}
--- TestApp/Utils.cs
+++ TestApp/Utils.cs
@@ -1,5 +1,23 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace TestApp {
     public class Utils {
-        
+        async public static Task<object> ParseBody(Stream stream) {
+            
+
+            using (var reader = new StreamReader(stream, Encoding.UTF8, true, 1024, true)) {
+                var bodyStr = await reader.ReadToEndAsync();
+                var body = JsonConvert.DeserializeObject(bodyStr);
+                        
+                if (stream.CanSeek)
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                return body;
+            }
+        }
     }
 }

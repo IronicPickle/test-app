f4834ec179f19cbf21362447b3bd68322d2678c6
Nathan Rath <nathan@silverchip.com>
2021-07-01 10:06:47 +0100

    Refactored react layout

---
+++ TestApp/test-app/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/app.scss";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h2 className="white">Test .NET and React App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
